90d93fb729078db13f2ed1c88d0a874c5804f3f6
ryan <xiaoyin_c@qq.com>
2018-05-28 12:15:27 +0800

    完成get_video_info

--- bilibili/bilibili.py
+++ bilibili/bilibili.py
@@ -6,63 +6,65 @@
 P.S. B站以前曾经使用过好几种链接，这里就只解析现在使用的这种了。（eg. ` bangumi.bilibili.com` `bilibili.tv`)
 用浏览器访问时，旧链接会被重定向到新链接。可以获取新链接再下载。
 """
-
+import hashlib
 import re
 import json
+from urllib import parse
+
 import requests
 
 import logging
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
-
-# headers 复制自 chrome
-headers = {
-    'Connection': 'keep-alive',
+# 复制自 chrome
+user_agent = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh,zh-CN;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,ja;q=0.5',
 }
 
 # b站的验证措施，需要用到这几个固定参数
 _APP_KEY = '84956560bc028eb7'
-_BILIBILI_KEY = '94aba54af9065f71de72f5508f1cd42e'
-_BILIBILI_KEY_2 = '9b288147e5474dd2aa67085f716c560d'  # 备用
+_BILIBILI_KEY = '94aba54af9065f71de72f5508f1cd42e'    # for av
+_BILIBILI_KEY_2 = '9b288147e5474dd2aa67085f716c560d'  # for bangumi
 
-api_url = 'http://interface.bilibili.com/v2/playurl?'
-bangumi_api_url = 'http://bangumi.bilibili.com/player/web_api/playurl?'
+# 视频清晰度, 复制自bilibili-helper
+QUALITY_DISPLAY_NAMES = {
+    116: '2k?',
+    112: '1080P+',
+    80: '1080P',
+    64: '720P',
+    48: '720P',
+    32: '480P',
+    16: '360P',
+}
 
-
-def get_download_url(cid):
-    """通过 interface.bilibili.com 的 api 获取视频源地址。"""
-    
-
+api_url = 'http://interface.bilibili.com/v2/playurl'
+bangumi_api_url = 'http://bangumi.bilibili.com/player/web_api/playurl'
 
 
 class BilibiliDownloader:
     """主类"""
     # 1. 动漫播放页
-    regex_ep = re.compile(r"https?://www\.bilibili\.com/bangumi/play/ep(?P<ep>\d+)")
+    regex_ep = re.compile(r"(?P<url>https?://www\.bilibili\.com/bangumi/play/ep(?P<ep>\d+))")
 
     # 2. 动漫首页
-    regex_home = re.compile(r"https?://www\.bilibili\.com/bangumi/media/md(?P<md>\d+)")
+    regex_home = re.compile(r"(?P<url>https?://www\.bilibili\.com/bangumi/media/md(?P<md>\d+))")
 
     # 3. av播放页
-    regex_p = re.compile(r"https?://www\.bilibili\.com/video/av(?P<avid>\d+)(?:/\?p=(?P<pname>\d+)|)")
+    regex_p = re.compile(r"(?P<url>https?://www\.bilibili\.com/video/av(?P<avid>\d+)(?:/\?p=(?P<pname>\d+)|))")
 
     def __init__(self, url):
         # 首先检测url是否符合要求，并提取出需要的信息
         url_type, self.args = self.type_check(url)
 
         self.__session = requests.Session()
-        self.__text = self.__session.get(url=url, headers=headers).text
+        self.__text = self.__session.get(url=self.args['url'], headers=user_agent).text  # 拿到给定网页
 
         self.parser = self.get_parser(url_type)
 
     def type_check(self, url):
         """use match，not fullmatch"""
-        match = self.regex_ep.match(url)   # 1. 是动漫的播放页？
+        match = self.regex_ep.match(url)  # 1. 是动漫的播放页？
         if match:
             return 'bangumi', match.groupdict()
 
@@ -70,7 +72,7 @@ class BilibiliDownloader:
         if match:
             return 'bangumi_home', match.groupdict()
 
-        match = self.regex_p.match(url)   # 3. 是up主上传的视频？
+        match = self.regex_p.match(url)  # 3. 是up主上传的视频？
         if match:
             return 'av', match.groupdict()
 
@@ -86,6 +88,29 @@ class BilibiliDownloader:
             return AvParser(self.__text)
         else:
             raise RuntimeError("url_type不可能为其他参数，请检查代码。")
+
+    def get_video_info(self, cid, quality):
+        """通过 interface.bilibili.com 的 api 获取视频源地址。"""
+        params = {
+            'appkey': _APP_KEY,
+            'cid': cid,
+            'otype': 'json',
+            'qn': quality,
+            'quality': quality,  # 这俩相同的参数指定视频质量
+            'type': '',  # 这个指定是flv还是mp4
+        }
+        params_str = parse.urlencode(params, encoding='utf-8')
+        sign = hashlib.md5(bytes(params_str + _BILIBILI_KEY, 'utf8')).hexdigest()  # 签名这一步，需要用到 _BILIBILI_KEY
+        params.update({'sign': sign})
+
+        headers = {
+            'refer': self.args['url'],
+        }
+        headers.update(user_agent)
+
+        resp = requests.get(url=api_url, params=params, headers=headers)
+
+        return resp.json()
 
 
 class AvParser:
@@ -139,7 +164,3 @@ class BangumiHomeParser:
         match = self.regex_episodes.search(self.text)
         json_str = match.group(1)
         return json.loads(json_str)
-
-
-
-
--- bilibili/test_bilibili.py
+++ bilibili/test_bilibili.py
@@ -2,6 +2,7 @@
 
 
 import unittest
+from pprint import pprint
 
 from utils import *
 from bilibili import BilibiliDownloader
@@ -19,6 +20,18 @@ class TestData:
                        'vid': '',
                        'weblink': ''},
             'page-num': 17,
+            'info-page-1': {'accept_description': ['高清 1080P', '高清 720P', '清晰 480P', '流畅 360P'],
+                            'accept_format': 'flv,hdmp4,flv480,mp4',
+                            'accept_quality': [80, 48, 32, 16],
+                            # 'durl': [],  # durl 每次请求都会有不同的结果
+                            'format': 'flv',
+                            'from': 'local',
+                            'quality': 80,
+                            'result': 'suee',
+                            'seek_param': 'start',
+                            'seek_type': 'offset',
+                            'timelength': 1102514,
+                            'video_codecid': 7}
         },
     ]
 
@@ -51,10 +64,16 @@ class TestBilibiliDowloader(unittest.TestCase):
         for item in TestData.test_av:
             downloader = get_downloader(item['url'])
             parser = downloader.parser
-            pages = parser.get_av_pages()
+            pages = parser.get_av_pages()  # 测试parser
 
+            # 确定parser工作正常
             self.assertEqual(len(pages), item['page-num'])
             self.assertEqual(pages[0], item['page-1'])
+
+            # 确定 get_video_info 工作正常
+            info = downloader.get_video_info(pages[0]['cid'], 116)  # 116 最高的清晰度参数
+            info.pop('durl')   # durl 每次请求都会有不同的结果
+            self.assertEqual(info, item['info-page-1'])
 
 
 
